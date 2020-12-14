00a3b90626add3e894e24dbab61935ad27c1836e
Jennifer Rosa <rosajen27@gmail.com>
2020-12-13 19:42:46 -0500

    create props and map functionality for note items

---
+++ src/notes.jsx
@@ -0,0 +1,19 @@
+const notes = [
+    {
+        key: 1,
+        title: "First Title",
+        content: "First Content"
+    },
+    {
+        key: 2,
+        title: "Second Title",
+        content: "Second Content"
+    },
+    {
+        key: 3,
+        title: "Third Title",
+        content: "Third Content"
+    }
+]
+
+export default notes;
